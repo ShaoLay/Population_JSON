697270c0b475b949551eac0ebe430e0440d94827
hamdi <javs_shao@163.com>
2019-01-25 15:34:30 +0800

    获取两个字母的国别码

--- country_codes.py
+++ country_codes.py
@@ -1,1 +1,18 @@
+from pygal_maps_world.i18n import COUNTRIES
 
+
+def get_country_code(country_name):
+    """
+    根据指定的国家, 返回Pygal使用的两个字母的国别码
+    :param country_name:
+    :return:
+    """
+    for code, name in COUNTRIES.items():
+        if name == country_name:
+            return code
+    # 如果没有找到指定的国家, 就返回None
+    return None
+
+print(get_country_code('Andorra'))
+print(get_country_code('United Arab Emirates'))
+print(get_country_code('Afghanistan'))
