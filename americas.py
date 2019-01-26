958a10a4006fc7e996976fea42a9f6b632537e5f
hamdi <javs_shao@163.com>
2019-01-26 14:31:15 +0800

    制作世界地图_1_突出美洲的地图

---
+++ americas.py
@@ -0,0 +1,11 @@
+import pygal_maps_world.maps
+
+
+wm = pygal_maps_world.maps.World()
+wm.title = 'North, Central, and South America'
+
+wm.add('North America', ['ca', 'mx', 'us'])
+wm.add('Central America', ['bz', 'cr', 'gt', 'hn', 'ni', 'sv'])
+wm.add('South America', ['ar', 'bo', 'br', 'cl', 'co', 'ec', 'gf',
+                         'gy', 'pe', 'py', 'uy', 've'])
+wm.render_to_file('americas.svg')
