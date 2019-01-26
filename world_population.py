8e03226eb95c1f03130c266f6a31b1f92dae415c
hamdi <javs_shao@163.com>
2019-01-26 14:45:34 +0800

    绘制完整的世界地图

--- world_population.py
+++ world_population.py
@@ -1,4 +1,5 @@
 import json
+import pygal_maps_world.maps
 
 
 # 将数据加载到一个列表中
@@ -8,6 +9,7 @@ filename = 'population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
 
+cc_populations = {}
 # 打印每个国家2010年的人口数量
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
@@ -15,6 +17,11 @@ for pop_dict in pop_data:
         population = int(float(pop_dict['Value']))
         code = get_country_code(country_name)
         if code:
-            print(code + ": " + str(population))
-        else:
-            print('Error - ' + country_name)
+            cc_populations[code] = population
+
+wm = pygal_maps_world.maps.World()
+wm.title = 'World Population in 2010, by Country'
+wm.add('2010', cc_populations)
+
+wm.render_to_file('world_population.svg')
+
